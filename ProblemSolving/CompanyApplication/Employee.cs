5cf59cf3ac36bc5eec87ca914794c42cf48b5463
Ahmed Abdalkader <84910261+Ahmedawad24@users.noreply.github.com>
2024-08-03 13:06:58 +0300

    Add files via upload

---
+++ ProblemSolving/CompanyApplication/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyApplication
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Wage { get; set; }
+        public double LoggedHours { get; set; }
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string Fname , string Lname , double Wage , double LoggedHours)
+        {
+            this.FirstName = Fname;
+            this.LastName = Lname;
+            this.Wage = Wage;
+            this.LoggedHours = LoggedHours;
+        }
+
+         public void CalculateNetSalary()
+        {
+            var NetSalary = Wage * LoggedHours - (Wage * LoggedHours * 0.03);
+            Console.WriteLine($"The Net Salary of {FirstName} {LastName} is {NetSalary}");
+        }
+
+    }
+
+}
---
+++ ProblemSolving/CompanyApplication/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace CompanyApplication
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+
+            const double TAX = 0.03;
+            string Fname;
+            string Lname;
+            double wage;
+            double LogHours;
+
+            bool isDone = true;
+
+            while (isDone) 
+            {
+                //Input of First and Last Name
+                Console.Write("First Name: ");
+                Fname = Console.ReadLine();
+                Console.Write("Last Name: ");
+                Lname = Console.ReadLine();
+                //Input Salary wage and logged hours
+                Console.Write($"Enter {Fname} {Lname} Wage: ");
+                wage = Convert.ToDouble(Console.ReadLine());
+                Console.Write($"Enter {Fname} {Lname} Logged Hours: ");
+                LogHours = Convert.ToDouble(Console.ReadLine());
+
+                Employee X = new Employee(Fname, Lname, wage, LogHours);
+                X.CalculateNetSalary();
+                Console.WriteLine("Press Enter for Continue and exit for Stop");
+
+                var E = Console.ReadKey();
+                if (E.Key != ConsoleKey.Enter)
+                {
+                    break;
+                }
+                Console.WriteLine("-------------------------------------------------");
+            }
+
+
+            ////Calculation of Net Salary with 3% Tax
+            //var NetSalary = wage * LogHours - (wage * LogHours * TAX);
+            //Console.WriteLine($"The net salary of {Fname} {Lname} is {NetSalary}");
+
+
+            Console.Read();
+
+            
+        }
+
+        
+    }
+}
